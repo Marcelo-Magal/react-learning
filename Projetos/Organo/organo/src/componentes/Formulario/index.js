157a73f9b7a3459cbd8c84b6739b0522bb05ce94
Marcelo Carvalho <marcelomagal@gmail.com>
2023-11-15 23:14:44 -0300

    Criado botão "Criar Card"

--- Projetos/Organo/organo/src/componentes/Formulario/index.js
+++ Projetos/Organo/organo/src/componentes/Formulario/index.js
@@ -1,5 +1,6 @@
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
+import Botao from "../Botao";
 import "./Formulario.css";
 
 const Formulario = () => {
@@ -14,15 +15,22 @@ const Formulario = () => {
     ' Inovação e Gestão'
     ];
     
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    console.log("Form foi enviado");
+  }
 
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador:</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-        <ListaSuspensa itens={times} label="Time" />
+        <CampoTexto obrigatorio={true} label="Nome" placeholde="Digite seu nome"/>
+        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo"/>
+        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+        <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+        <Botao>
+          Criar card 
+        </Botao>
       </form>
     </section>
   );
